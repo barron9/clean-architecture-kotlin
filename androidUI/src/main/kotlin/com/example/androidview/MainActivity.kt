ebe4cd644851e8beef8fa7f8e2afac1bc5d73fbe
berkin tatlısu <barron9@yandex.com>
2021-09-26 18:08:15 +0300

    navigation comp. added

--- androidUI/src/main/kotlin/com/example/androidview/MainActivity.kt
+++ androidUI/src/main/kotlin/com/example/androidview/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidview
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.cachedata.DataCacheFactory
 import com.di.DaggerAppComp
 import com.example.androidview.databinding.ActivityMainBinding
@@ -12,7 +13,7 @@ import io.reactivex.schedulers.Schedulers
 import usecases.DefaultUseCase
 import javax.inject.Inject
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var dcf: DataCacheFactory
 
